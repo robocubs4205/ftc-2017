ea673e86bc5b2c0778f3f88e4c944fcbe41a271d
Trevor <tgiddings000@gmail.com>
2017-10-24 20:06:08 -0700

    Added controls for glyph manipulator and idol arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeOpMode.java
@@ -12,6 +12,10 @@ import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
 @TeleOp(name="TeleOp")
 public class ArcadeOpMode extends RoboCubsOpMode {
+    final double drivePowerScale = 0.8;
+    final double liftPowerScale = 1;
+    final double armPowerScale = 1.0;
+
     @Override
     public void loop() {
         //Positive is forward
@@ -22,7 +26,42 @@ public class ArcadeOpMode extends RoboCubsOpMode {
         double leftPower = forwardPower - turnPower;
         double rightPower = forwardPower + turnPower;
 
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+        leftDrive.setPower(leftPower * drivePowerScale);
+        rightDrive.setPower(rightPower * drivePowerScale);
+
+        double liftMotorPower = 0;
+        if (gamepad2.b){
+            liftMotorPower = 1;
+        }
+        else if (gamepad2.a && !liftLowerLimit.isPressed()){
+            liftMotorPower = -1;
+
+        }
+        liftMotor.setPower(liftMotorPower * liftPowerScale);
+
+        double armMotorPower = gamepad2.right_stick_y;
+        armMotor.setPower(armMotorPower * armPowerScale);
+        //Negative is open
+        double glyphClampPosition = glyphClamp.getPosition();
+        if (gamepad2.y){
+            glyphClampPosition = -1;
+        }
+        else if (gamepad2.x){
+            glyphClampPosition = 1;
+        }
+        glyphClamp.setPosition(glyphClampPosition);
+
+        //Positive is up
+        double hookerPosition = hooker.getPosition();
+        if(gamepad2.right_bumper) {
+            hookerPosition = 1;
+        }
+        else if (gamepad2.right_trigger > 0.5) {
+            hookerPosition = -1;
+        }
+        
+        hooker.setPosition(hookerPosition);
     }
+
+
 }
