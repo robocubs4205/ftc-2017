7a1151c54434417f00dea182d2dac08a341683a1
Trevor <tgiddings000@gmail.com>
2017-11-01 20:01:12 -0700

    Implemented MoveForwardAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveForwardAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveForwardAuto.java
@@ -1,11 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@Autonomous(name = "Move forward")
 public class MoveForwardAuto extends LinearOpMode {
+    @SuppressWarnings("FieldCanBeLocal")
+    private final double drivePower = 0.75;
     private Robot robot = new Robot();
     @Override
     public void runOpMode() throws InterruptedException {
-
+        robot.init(hardwareMap);
+        waitForStart();
+        robot.leftDrive.setPower(drivePower);
+        robot.rightDrive.setPower(drivePower);
+        sleep((long) (robot.timeForDistance(2, drivePower) * 1000));
     }
 }
