283ade37f1b7949e9b82049d2044e271b334a066
Trevor <tgiddings000@gmail.com>
2017-11-04 13:21:23 -0700

    Change to Tank drive. Reduce speed of arm extension.  Change glyph clamp and hooker to move while buttons are held.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeOpMode.java
@@ -10,7 +10,7 @@ public class ArcadeOpMode extends OpMode {
 
     private final double drivePowerScale = 0.8;
     private final double liftPowerScale = 1;
-    private final double armExtendPowerScale = 1.0;
+    private final double armExtendPowerScale = 0.6;
     private final double armLiftPowerScale = 1.0;
 
     @Override
@@ -20,13 +20,13 @@ public class ArcadeOpMode extends OpMode {
 
     @Override
     public void loop() {
-        //Positive is forward
-        double forwardPower = gamepad1.left_stick_y;
-        //Positive is left
-        double turnPower = gamepad1.left_stick_x;
+        //Left Positive Power
+        double leftForwardPower = gamepad1.left_stick_y;
+        //Right Positive Power
+        double rightForwardPower = gamepad1.right_stick_y;
 
-        double leftPower = forwardPower - turnPower;
-        double rightPower = forwardPower + turnPower;
+        double leftPower = leftForwardPower;
+        double rightPower = rightForwardPower;
 
         robot.leftDrive.setPower(leftPower * drivePowerScale);
         robot.rightDrive.setPower(rightPower * drivePowerScale);
@@ -46,22 +46,29 @@ public class ArcadeOpMode extends OpMode {
         //Negative is open
         double glyphClampPosition = robot.glyphClamp.getPosition();
         if (gamepad2.y){
-            glyphClampPosition = -1;
+            glyphClampPosition += 0.01;
+            if (glyphClampPosition > 1.0 )
+                glyphClampPosition = 1;
         }
         else if (gamepad2.x){
-            glyphClampPosition = 1;
+            glyphClampPosition -= 0.01;
+            if (glyphClampPosition < 0 )
+                glyphClampPosition = 0;
         }
         robot.glyphClamp.setPosition(glyphClampPosition);
 
         //Positive is up
         double hookerPosition = robot.hooker.getPosition();
         if(gamepad2.right_bumper) {
-            hookerPosition = 1;
+            hookerPosition += 0.01;
+            if (hookerPosition > 1.0 )
+                hookerPosition = 1;
         }
         else if (gamepad2.right_trigger > 0.5) {
-            hookerPosition = -1;
+            hookerPosition -= 0.01;
+            if (hookerPosition < 0 )
+                hookerPosition = 0;
         }
-
         robot.hooker.setPosition(hookerPosition);
 
         double armLiftPower = gamepad2.left_stick_y;
