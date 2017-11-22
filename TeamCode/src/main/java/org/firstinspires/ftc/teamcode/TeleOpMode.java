8489285a04adb03e0828de53c3f278f9d4e2f9e7
Trevor <tgiddings000@gmail.com>
2017-11-21 20:06:23 -0800

    Change teleop to use new robot objects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -7,10 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="TeleOp")
 public class TeleOpMode extends OpMode {
     private Robot robot = new Robot();
-
-    private final double liftPowerScale = 1;
-    private final double armExtendPowerScale = 0.6;
-    private final double armLiftPowerScale = 1.0;
+    double dt = 0;
 
     @Override
     public void init() {
@@ -20,51 +17,36 @@ public class TeleOpMode extends OpMode {
 
     @Override
     public void loop() {
-        robot.leftDrive.setPower(Robot.motorPowerCurve(gamepad1.left_stick_y));
-        robot.rightDrive.setPower(Robot.motorPowerCurve(gamepad1.right_stick_y));
+        long startTime = System.nanoTime();
+        robot.drive.fromJoysticks(gamepad1);
 
-        double liftMotorPower = 0;
         if (gamepad2.b){
-            liftMotorPower = 1;
+            robot.glypher.raise(1);
         }
-        else if (gamepad2.a && !robot.liftLowerLimit.isPressed()){
-            liftMotorPower = -1;
-
+        else if (gamepad2.a){
+            robot.glypher.lower(1);
         }
-        robot.liftMotor.setPower(liftMotorPower * liftPowerScale);
 
         double armExtendPower = gamepad2.right_stick_y;
-        robot.armExtend.setPower(armExtendPower * armExtendPowerScale);
-        //Negative is open
-        double glyphClampPosition = robot.glyphClamp.getPosition();
+        robot.arm.extend(armExtendPower);
+
         if (gamepad2.y){
-            glyphClampPosition += 0.01;
-            if (glyphClampPosition > 1.0 )
-                glyphClampPosition = 1;
+            robot.glypher.open(1, dt);
         }
         else if (gamepad2.x){
-            glyphClampPosition -= 0.01;
-            if (glyphClampPosition < 0 )
-                glyphClampPosition = 0;
+            robot.glypher.close(1, dt);
         }
-        robot.glyphClamp.setPosition(glyphClampPosition);
 
-        //Positive is closed
-        double hookerPosition = robot.hooker.getPosition();
         if(gamepad2.right_bumper) {
-            hookerPosition += 0.01;
-            if (hookerPosition > 1.0 )
-                hookerPosition = 1;
+            robot.hooker.open(1, dt);
         }
         else if (gamepad2.right_trigger > 0.5) {
-            hookerPosition -= 0.01;
-            if (hookerPosition < 0 )
-                hookerPosition = 0;
+            robot.hooker.close(1, dt);
         }
-        robot.hooker.setPosition(hookerPosition);
 
-        double armLiftPower = gamepad2.left_stick_y;
-        robot.armLift.setPower(armLiftPower * armLiftPowerScale);
+        robot.arm.raise(gamepad2.left_stick_y);
+
+        dt = ((double) (System.nanoTime() - startTime)) / 1000000000;
     }
 
 
